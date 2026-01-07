442542740aef03b5422023e8444d946de7d5cae0
kartiknoriya <148875248+kartiknoriya@users.noreply.github.com>
2026-01-07 19:07:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,15 +4,21 @@ const HOVER_COLOR = '#6F4E37';
 const DEFAULT_COLOR = '#E6E6FA';
 
 boxes.forEach((box) => {
-  box.addEventListener('mouseenter', () => {
+  box.addEventListener('mouseover', () => {
     boxes.forEach(b => {
-      if (b !== box) b.style.backgroundColor = HOVER_COLOR;
+      if (b !== box) {
+        b.style.backgroundColor = HOVER_COLOR;
+      }
     });
   });
 
-  box.addEventListener('mouseleave', () => {
+  box.addEventListener('mouseout', () => {
     boxes.forEach(b => {
-      if (b !== box) b.style.backgroundColor = DEFAULT_COLOR;
+      if (b !== box) {
+        b.style.backgroundColor = DEFAULT_COLOR;
+      }
     });
   });
+});
+
 });
