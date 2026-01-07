8a6c7d871b7fcb418666f2139bddb67c84c9cf17
kartiknoriya <148875248+kartiknoriya@users.noreply.github.com>
2026-01-07 19:12:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
-const boxes = document.querySelectorAll('.square');
+document.addEventListener("DOMContentLoaded", function () {
+  const boxes = document.querySelectorAll(".square");
 
-const HOVER_COLOR = '#6F4E37';
-const DEFAULT_COLOR = '#E6E6FA';
+  const HOVER_COLOR = "#6F4E37";
+  const DEFAULT_COLOR = "#E6E6FA";
 
-boxes.forEach((box) => {
-  box.addEventListener('mouseover', () => {
-    boxes.forEach(b => {
-      if (b !== box) {
-        b.style.backgroundColor = HOVER_COLOR;
-      }
+  boxes.forEach(function (box) {
+    box.addEventListener("mouseover", function () {
+      boxes.forEach(function (b) {
+        if (b !== box) {
+          b.style.backgroundColor = HOVER_COLOR;
+        }
+      });
+    });
+
+    box.addEventListener("mouseout", function () {
+      boxes.forEach(function (b) {
+        if (b !== box) {
+          b.style.backgroundColor = DEFAULT_COLOR;
+        }
+      });
     });
   });
-
-  box.addEventListener('mouseout', () => {
-    boxes.forEach(b => {
-      if (b !== box) {
-        b.style.backgroundColor = DEFAULT_COLOR;
-      }
-    });
-  });
-});
-
 });
